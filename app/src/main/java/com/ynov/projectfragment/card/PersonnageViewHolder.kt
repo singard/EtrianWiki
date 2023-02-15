a9c341fdafd20a92db05da85400c535f0ab4a8bf
singard <mathieugonza3@gmail.com>
2023-02-15 17:45:35 +0100

    add image to cardView

--- app/src/main/java/com/ynov/projectfragment/card/PersonnageViewHolder.kt
+++ app/src/main/java/com/ynov/projectfragment/card/PersonnageViewHolder.kt
@@ -1,6 +1,7 @@
 package com.ynov.projectfragment.card
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.ynov.projectfragment.R
@@ -11,12 +12,17 @@ class PersonnageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val textViewProfile: TextView = itemView.findViewById(R.id.profile)
     private val textViewStrengths: TextView = itemView.findViewById(R.id.strengths)
     private val textViewWeaknesses: TextView = itemView.findViewById(R.id.weaknesses)
+    private val imgIlu: ImageView = itemView.findViewById(R.id.imageViewJob)
 
     fun bind(characterClass: CharacterClass) {
         textViewName.text = characterClass.name
         textViewProfile.text = characterClass.profile
         textViewStrengths.text = characterClass.strengths
         textViewWeaknesses.text = characterClass.weaknesses
+
+        val imageName = characterClass.imgId
+        val resId = itemView.context.resources.getIdentifier(imageName, "drawable", itemView.context.packageName)
+        imgIlu.setImageResource(resId)
     }
 }
 
