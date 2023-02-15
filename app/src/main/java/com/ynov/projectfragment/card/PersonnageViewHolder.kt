6473a7e94ff6e2187fcec8a9e8c9a1a63b12a3d3
singard <mathieugonza3@gmail.com>
2023-02-15 21:23:32 +0100

    create activity in card

--- app/src/main/java/com/ynov/projectfragment/card/PersonnageViewHolder.kt
+++ app/src/main/java/com/ynov/projectfragment/card/PersonnageViewHolder.kt
@@ -1,9 +1,12 @@
 package com.ynov.projectfragment.card
 
+import android.content.Intent
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.ynov.projectfragment.JobDetailActivity
 import com.ynov.projectfragment.R
 import com.ynov.projectfragment.model.CharacterClass
 //cardview
@@ -13,6 +16,7 @@ class PersonnageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val textViewStrengths: TextView = itemView.findViewById(R.id.strengths)
     private val textViewWeaknesses: TextView = itemView.findViewById(R.id.weaknesses)
     private val imgIlu: ImageView = itemView.findViewById(R.id.imageViewJob)
+    private val cardView: CardView = itemView.findViewById(R.id.cardViewJob) // pour le clic card
 
     fun bind(characterClass: CharacterClass) {
         textViewName.text = characterClass.name
@@ -23,6 +27,12 @@ class PersonnageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imageName = characterClass.imgId
         val resId = itemView.context.resources.getIdentifier(imageName, "drawable", itemView.context.packageName)
         imgIlu.setImageResource(resId)
+        //pour le clic card
+        cardView.setOnClickListener {
+            val intent = Intent(itemView.context, JobDetailActivity::class.java)
+            intent.putExtra("characterClass", characterClass)
+            itemView.context.startActivity(intent)
+        }
     }
 }
 
